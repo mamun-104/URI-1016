20a322f86319aefc778d104ed4ac54332b1436ef
Md. Ibrahim Mamun <67334936+mamun-104@users.noreply.github.com>
2021-05-07 05:14:29 +0600

    Add files via upload

---
+++ 1016.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<iomanip>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin>>x;
+
+    int result;
+
+    result = x/0.5;
+    cout<<result<<" "<<"minutos"<<endl;
+
+
+
+
+    return 0;
+}
+
+
